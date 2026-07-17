2cd3831269fc678af9ec8cf6d886290e301f053a
Prekshith7337 <krprekshith@gmail.com>
2026-07-17 08:41:42 +0530

    Create orders.py

---
+++ bot/bot/bot/orders.py
@@ -0,0 +1,22 @@
+from bot.client import get_client
+
+def place_order(symbol, side, order_type, quantity, price=None):
+    client = get_client()
+
+    if order_type == "MARKET":
+        return client.futures_create_order(
+            symbol=symbol,
+            side=side,
+            type="MARKET",
+            quantity=quantity
+        )
+
+    elif order_type == "LIMIT":
+        return client.futures_create_order(
+            symbol=symbol,
+            side=side,
+            type="LIMIT",
+            quantity=quantity,
+            price=price,
+            timeInForce="GTC"
+        )
